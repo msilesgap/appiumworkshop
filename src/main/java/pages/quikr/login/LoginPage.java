ef440ba49fd910dc1b4318007edf1c2fee970088
Moises Siles <msiles@growthaccelerationpartners.com>
2019-04-23 00:37:50 -0600

    Fix locator issue

--- src/main/java/pages/quikr/login/LoginPage.java
+++ src/main/java/pages/quikr/login/LoginPage.java
@@ -10,19 +10,7 @@ import utility.ActionBot;
 public class LoginPage extends PageObjectBase {
     AppiumDriver appiumDriver;
 
-    /*waitForElementVisible(By.id("com.quikr:id/action_button_skip"), 20);
-    findElement(By.id("com.quikr:id/action_button_skip")).click();
-
-    waitForElementVisible(By.id("com.quikr:id/login_register_view"), 20);
-    //findElement(By.id("com.quikr:id/login_register_view")).sendKeys("my user");
-    findElement(By.id("com.quikr:id/login_register_view")).click();
-
-    waitForElementVisible(By.id("com.quikr:id/email_mobile"), 20);
-    findElement(By.id("com.quikr:id/email_mobile")).sendKeys("test@wearegap.com");
-    findElement(By.id("com.quikr:id/login_password")).sendKeys("123456");
-    findElement(By.id("com.quikr:id/login_button")).click();*/
-
-    @FindBy(id="com.quikr:id/email_mobilex")
+    @FindBy(id="com.quikr:id/email_mobile")
     private WebElement email;
 
     @FindBy(id="com.quikr:id/login_password")
