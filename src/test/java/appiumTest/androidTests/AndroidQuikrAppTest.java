508fbb67434202d5b2a0dc8f3d0fe04e43488870
Moises Siles <msiles@growthaccelerationpartners.com>
2019-04-23 00:33:56 -0600

    Remove unused code

--- src/test/java/appiumTest/androidTests/AndroidQuikrAppTest.java
+++ src/test/java/appiumTest/androidTests/AndroidQuikrAppTest.java
@@ -1,7 +1,6 @@
 package appiumTest.androidTests;
 
 import appiumTest.androidTests.config.TestCase;
-import org.openqa.selenium.ScreenOrientation;
 import org.testng.annotations.Test;
 import pages.quikr.login.LoginPage;
 import pages.quikr.login.LoginRegisterPage;
@@ -11,21 +10,6 @@ import utility.ActionBot;
 import static io.appium.java_client.touch.offset.PointOption.point;
 
 public class AndroidQuikrAppTest extends TestCase {
-    //AppiumDriver driver;
-
-    /*@BeforeMethod
-    public void setUp() throws MalformedURLException {
-        //Set up desired capabilities and pass the Android app-activity and app-package to Appium
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("app","/Users/moisessiles/GAPProjects/AppiumWorkshop/src/main/resources/quikr-8-86.apk");
-
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, Utils.getProperty("androidDevice"));
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Utils.getProperty("androidPlatformName"));
-        capabilities.setCapability("noReset", false);
-        capabilities.setCapability("fullReset",  true);
-
-        driver = new AndroidDriver(new URL(Utils.getProperty("AppiumURL")), capabilities);
-    }*/
 
     /*@Test
     public void quikrSwipeGestureTest() {
@@ -63,27 +47,6 @@ public class AndroidQuikrAppTest extends TestCase {
         LoginPage loginPage = loginRegisterPage.clickLoginRegisterText();
         loginPage.fillRegisterForm("test@wearegap.com", "123456");
         loginPage.clickLoginButton();
-
-
-
-
-
-        /*waitForElementVisible(By.id("com.quikr:id/action_button_skip"), 20);
-        findElement(By.id("com.quikr:id/action_button_skip")).click();
-
-        waitForElementVisible(By.id("com.quikr:id/login_register_view"), 20);
-        //findElement(By.id("com.quikr:id/login_register_view")).sendKeys("my user");
-        findElement(By.id("com.quikr:id/login_register_view")).click();
-
-        waitForElementVisible(By.id("com.quikr:id/email_mobile"), 20);
-        findElement(By.id("com.quikr:id/email_mobile")).sendKeys("test@wearegap.com");
-        findElement(By.id("com.quikr:id/login_password")).sendKeys("123456");
-        findElement(By.id("com.quikr:id/login_button")).click();*/
     }
 
-   /* @AfterMethod
-    public void teardown() {
-        //close the app
-        driver.quit();
-    }*/
 }
